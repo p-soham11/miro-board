436ffb5eb89fd3914d146f7bad55bacc1ea4c2d7
Soham Pal <sohampal1108@gmail.com>
2024-06-29 00:54:19 +0530

    Adding Layer Functionality

--- app/board/[boardId]/_components/selectionTools.tsx
+++ app/board/[boardId]/_components/selectionTools.tsx
@@ -10,7 +10,7 @@ import { ColorPicker } from "./colorPicker";
 import { useDeleteLayers } from "@/hooks/useDeleteLayers";
 import { Hint } from "@/components/hint";
 import { Button } from "@nextui-org/button";
-import { Trash } from "lucide-react";
+import { Trash, PanelBottomOpen, PanelTopOpen } from "lucide-react";
 
 interface SelectionToolsProps {
     camera: Camera;
@@ -20,6 +20,47 @@ interface SelectionToolsProps {
 export const SelectionTools = memo(
     ({ camera, setLastUsedColor }: SelectionToolsProps) => {
         const selection = useSelf((me) => me.presence.selection);
+
+        const moveToBack = useMutation(
+            ({ storage }) => {
+                const liveLayerIds = storage.get("layerIds");
+                const indices: number[] = [];
+
+                const arr = liveLayerIds.toArray();
+                for (let i = 0; i < arr.length; i++) {
+                    if (selection.includes(arr[i])) {
+                        indices.push(i);
+                    }
+                }
+
+                for (let i = 0; i < indices.length; i++) {
+                    liveLayerIds.move(indices[i], i);
+                }
+            },
+            [selection]
+        );
+
+        const bringToFront = useMutation(
+            ({ storage }) => {
+                const liveLayerIds = storage.get("layerIds");
+                const indices: number[] = [];
+
+                const arr = liveLayerIds.toArray();
+                for (let i = 0; i < arr.length; i++) {
+                    if (selection.includes(arr[i])) {
+                        indices.push(i);
+                    }
+                }
+
+                for (let i = indices.length - 1; i >= 0; i--) {
+                    liveLayerIds.move(
+                        indices[i],
+                        arr.length - 1 - (indices.length - 1 - i)
+                    );
+                }
+            },
+            [selection]
+        );
 
         const setFill = useMutation(
             ({ storage }, fill: Color) => {
@@ -52,12 +93,28 @@ export const SelectionTools = memo(
                 }}
             >
                 <ColorPicker onChange={setFill} />
-                <div className="flex items-center ml-2  border-neutral-200 max-w-[45px] ">
+                <div className="flex flex-col gap-y-0.5">
+                    <Hint label="Move to top" side="right" sideOffset={15}>
+                        <Button
+                            variant="light"
+                            size="sm"
+                            onClick={bringToFront}
+                        >
+                            <PanelBottomOpen />
+                        </Button>
+                    </Hint>
+                    <Hint label="Move to back" side="right" sideOffset={15}>
+                        <Button variant="light" size="sm" onClick={moveToBack}>
+                            <PanelTopOpen />
+                        </Button>
+                    </Hint>
+                </div>
+                <div className="flex items-center ml-2 border-l border-neutral-200 ">
                     <Hint label="Delete" side="right" sideOffset={15}>
                         <Button
                             variant="light"
                             size="sm"
-                            className=" -ml-3"
+                            className=" ml-2 -mr-1"
                             onClick={deleteLayers}
                         >
                             <Trash />
