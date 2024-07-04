cd7b3e38a1b70bb385368ce2dfe063bc8eab2d0e
Soham Pal <sohampal1108@gmail.com>
2024-07-05 00:46:03 +0530

    Adding Real-time pencil sketch

--- app/board/[boardId]/_components/cursorsPresence.tsx
+++ app/board/[boardId]/_components/cursorsPresence.tsx
@@ -3,8 +3,11 @@
 "use client";
 
 import { memo } from "react";
-import { useOthersConnectionIds } from "@/liveblocks.config";
+import { useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
 import { Cursor } from "./cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./path";
+import { colorToCss } from "@/lib/utils";
 
 const Cursors = () => {
     const ids = useOthersConnectionIds();
@@ -18,9 +21,43 @@ const Cursors = () => {
     );
 };
 
+const Drafts = () => {
+    const others = useOthersMapped(
+        (other) => ({
+            pencilDraft: other.presence.pencilDraft,
+            penColor: other.presence.penColor,
+        }),
+        shallow
+    );
+    return (
+        <>
+            {others.map(([key, other]) => {
+                if (other.pencilDraft) {
+                    return (
+                        <Path
+                            key={key}
+                            x={0}
+                            y={0}
+                            points={other.pencilDraft}
+                            fill={
+                                other.penColor
+                                    ? colorToCss(other.penColor)
+                                    : "1e1e1e"
+                            }
+                        />
+                    );
+                }
+
+                return null;
+            })}
+        </>
+    );
+};
+
 export const CursorsPresence = memo(() => {
     return (
         <>
+            <Drafts />
             <Cursors />
         </>
     );
