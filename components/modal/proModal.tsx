5ddb377e15e14911d1309a27437cdcf3e5d75fb9
p-soham11 <sohampal1108@gmail.com>
2024-07-17 02:06:23 +0530

    Adding Modal for Pro Plan

--- components/modal/proModal.tsx
+++ components/modal/proModal.tsx
@@ -2,34 +2,53 @@
 
 "use client";
 
-import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription,
-    DialogClose,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useProModal } from "@/store/useProModal";
 import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
-import { FormEventHandler, useEffect, useState } from "react";
-import { toast } from "sonner";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+});
 
 export const ProModal = () => {
     const { isOpen, onClose } = useProModal();
 
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>✨ Pro Modal</DialogTitle>
-                </DialogHeader>
-                <DialogDescription>
-                    Board Limit has been reached. Please upgrade to Pro to
-                    create more boards.
-                </DialogDescription>
+            <DialogContent className="max-w-[372px] p-0 overflow-hidden">
+                <div className="aspect-video relative flex items-center justify-center m-2">
+                    <Image
+                        src="/pro.svg"
+                        alt="Upgrade to Pro Plan"
+                        className="object-fit"
+                        fill
+                    />
+                </div>
+                <div
+                    className={cn(
+                        "text-neutral-700 mx-auto space-y-6 p-6",
+                        font.className
+                    )}
+                >
+                    <h2 className="font-medium text-lg">
+                        ⚡️ Upgrade to Pro Plan!
+                    </h2>
+                    <div className="pl-3">
+                        <ul className="text-[12px] space-y-1 list-disc">
+                            <li>Unlimited Boards</li>
+                            <li>Unlimited Organizations</li>
+                            <li>Unlimited Members</li>
+                            <li>Unlimited Layers</li>
+                        </ul>
+                    </div>
+                    <Button color="primary" className="w-full">
+                        Upgrade
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     );
